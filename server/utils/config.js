e7b874c22d5627bb136afa3931e8930294ca03cc
Pauli Sormunen <pauli.sormunen@outlook.com>
2022-02-24 23:20:12 +0200

    set default port

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,6 +4,6 @@ let MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 3003
 
 module.exports = { MONGODB_URI, PORT }
